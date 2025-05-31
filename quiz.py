b5c6457a7126bbb1b372ae2e04c22104218b35be
Rayana <rayanalimag@gmail.com>
2025-05-31 18:25:05 -0300

    fix

--- quiz.py
+++ quiz.py
@@ -1,17 +1,17 @@
 print("----The BigBang Theory----")
 print("Bem vindo ao quiz!")
-answer_user = input("Vamos começar? (S/N)")
+resposta_usuario = input("Vamos começar? (S/N)")
 
-if answer_user != "S":
+if resposta_usuario != "S":
     quit()
 
 score = 0
 
 print("Começando...")
 print("1-Qual é a origem do termo Bazinga?\n (A)  Sheldon inventou \n (B) Veio de um programa infantil \n (C) É a forma que sheldon usa pra indicar sarcasmo \n (D) Bordão do Leonard \n")
-answer_1 = input("Resposta: ")
+resposta_1 = input("Resposta: ")
 
-if answer_1 == "C":
+if resposta_1 == "C":
     print("Correto!")
     score = score + 1
 else:
@@ -19,9 +19,9 @@ else:
 
 
 print("2-Qual foi o nome do primeiro namorado da Penny?\n (A) Mike \n (B) Doug \n (C)Kurt \n (D) Zack \n")
-answer_1 = input("Resposta: ")
+resposta_2 = input("Resposta: ")
 
-if answer_1 == "C":
+if resposta_2 == "C":
     print("Correto!")
     score = score + 1
 else:
@@ -29,9 +29,9 @@ else:
 
 
 print("3-Em qual universidade os personagens principais trabalham?\n (A) MIT \n (B) Caltech \n (C) Stanford \n (D) Ucla \n")
-answer_1 = input("Resposta: ")
+resposta_3 = input("Resposta: ")
 
-if answer_1 == "B":
+if resposta_3 == "B":
     print("Correto!")
     score = score + 1
 else:
@@ -39,9 +39,9 @@ else:
 
 
 print("4-Em que cidade se passa a série\n (A) San Francisco \n (B) Pasadena \n (C) Boston \n (D) Austin \n")
-answer_1 = input("Resposta: ")
+resposta_4 = input("Resposta: ")
 
-if answer_1 == "B":
+if resposta_4 == "B":
     print("Correto!")
     score = score + 1
 else:
@@ -49,9 +49,9 @@ else:
 
 
 print("5-Qual desses personagens NÃO possui doutorado\n (A) Raj \n (B) Leonard \n (C) Sheldon \n (D) Howard \n")
-answer_1 = input("Resposta: ")
+resposta_5 = input("Resposta: ")
 
-if answer_1 == "D":
+if resposta_5 == "D":
     print("Correto!")
     score = score + 1
 else:
